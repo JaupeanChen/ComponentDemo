6caf55df01e1878ff0685d98f845529d81b132d8
Jarvan <1129561405@qq.com>
2021-09-26 17:53:26 +0800

    组件更新，处理toolbar问题

--- app/src/main/java/com/ylz/componentdemo/MainActivity.java
+++ app/src/main/java/com/ylz/componentdemo/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     //因为BaseActivity中我用了toolbar，但是如果将主题改为NoActionBar的话，就会出现toolbar空指针的问题
     //后来发现BaseActivity判断supportActionBar不为空错了，应该是为空，但是改了还是空指针。。
     //但是有个kotlin的应用跑来就完全没问题，不知道是不是toolbar实例获取的问题
+    //最后发现只是初始化的时候把setContentView顺序放错，放在初始化toolbar后面了...
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
